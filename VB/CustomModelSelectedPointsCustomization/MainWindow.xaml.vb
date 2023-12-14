9051cd6edaaec495ea38b0b9e422fce280bc6c10
DevExpressExampleBot <fakeEmail>
2023-12-14 16:15:55 +0400

    Source auto update [skip ci]

--- VB/CustomModelSelectedPointsCustomization/MainWindow.xaml.vb
+++ VB/CustomModelSelectedPointsCustomization/MainWindow.xaml.vb
@@ -1,31 +1,32 @@
-﻿Imports System.Globalization
+Imports System.Globalization
 Imports System.Windows
 Imports System.Windows.Data
 Imports System.Windows.Media
 
 Namespace CustomModelSelectedPointsCustomization
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 
-	Public Class IsSelectedToBrushConverter
-		Implements IValueConverter
+    Public Class IsSelectedToBrushConverter
+        Implements IValueConverter
 
-		Public Function Convert(ByVal value As Object, ByVal targetType As System.Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
-			If TypeOf value Is Boolean AndAlso targetType Is GetType(Brush) Then
-				Dim isSelected As Boolean = DirectCast(value, Boolean)
-				Return If(isSelected, Brushes.Black, Brushes.Red)
-			End If
-			Return Nothing
-		End Function
+        Public Function Convert(ByVal value As Object, ByVal targetType As System.Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+            If TypeOf value Is Boolean AndAlso targetType Is GetType(Brush) Then
+                Dim isSelected As Boolean = CBool(value)
+                Return If(isSelected, Brushes.Black, Brushes.Red)
+            End If
 
-		Public Function ConvertBack(ByVal value As Object, ByVal targetType As System.Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
-			Throw New System.NotImplementedException()
-		End Function
-	End Class
+            Return Nothing
+        End Function
+
+        Public Function ConvertBack(ByVal value As Object, ByVal targetType As System.Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Throw New System.NotImplementedException()
+        End Function
+    End Class
 End Namespace
